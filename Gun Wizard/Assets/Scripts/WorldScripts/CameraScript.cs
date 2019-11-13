efdbe483ac3000fa2364bf6b443aea77a24596d5
Redzango <cjohns10@trinity.edu>
2019-11-13 17:53:02 -0600

    added scene switching

---
+++ Gun Wizard/Assets/Scripts/WorldScripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.transform.position = Player.GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(Player.GetComponent<Transform>().position.x, Player.GetComponent<Transform>().position.y, gameObject.transform.position.z);
+    }
+}

f84b3c502fc7f18911624c7d882dd9c48e62311f
Nihil <npatel5@trinity.edu>
2019-11-19 11:05:15 -0600

    Add arrow functionality to Battle Scene

---
+++ Gun Wizard/Assets/Scripts/ElementButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ElementButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject arrow1;
+    private GameObject arrow2;
+    void Start()
+    {
+        arrow1 = transform.Find("Arrow1").gameObject;
+        arrow2 = transform.Find("Arrow2").gameObject;
+        arrow1.SetActive(false);
+        arrow2.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+
+    public void showArrows()
+    {
+        Debug.Log("Displaying Arrows.");
+        arrow1.SetActive(true);
+        arrow2.SetActive(true);
+    }
+
+    public void hideArrows()
+    {
+        Debug.Log("Hiding Arrows.");
+        arrow1.SetActive(false);
+        arrow2.SetActive(false);
+    }
+}

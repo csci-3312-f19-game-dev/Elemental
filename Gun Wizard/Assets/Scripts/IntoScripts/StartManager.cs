c1fb7f430cad388851e0275cf25030e78bbc9e6e
Redzango <cjohns10@trinity.edu>
2019-11-30 18:31:03 -0600

    added start scene and introduction scene with complete scene changes

---
+++ Gun Wizard/Assets/Scripts/IntoScripts/StartManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class StartManager : MonoBehaviour
+{
+    public Button startButton;
+    public Button introButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startButtonPress()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void introButtonPress()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
